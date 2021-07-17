1af31218ea4bb9ec579b7121ddedca0c2a0580e1
Björn Tirsén <bjorntirsen@gmail.com>
2021-07-17 14:39:47 +0200

    :sparkles: Add project cards

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,3 +1,8 @@
-exports.renderIndex = (req, res) => {
-  res.render('index', { title: 'Hello World!' });
-};
+const catchAsync = require('../utils/catchAsync');
+//const AppError = require('../utils/appError');
+const Project = require('../models/projectModel');
+
+exports.renderIndex = catchAsync(async (req, res) => {
+  const projects = await Project.find();
+  res.render('index', { title: 'Hello World!', projects });
+});
