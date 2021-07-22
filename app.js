86b094cd098a3d8e3b9709c1e80de1e3d997e70f
Björn Tirsén <bjorntirsen@gmail.com>
2021-07-22 20:53:44 +0200

    :rocket: Prepare for deploy

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const path = require('path');
 const express = require('express');
 const expressEjsLayout = require('express-ejs-layouts');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 const cookieParser = require('cookie-parser');
+const xss = require('xss-clean');
+const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -16,6 +20,9 @@ app.enable('trust proxy');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(cors());
+app.options('*', cors());
 
 // Global Middleware
 app.use(expressEjsLayout);
@@ -31,9 +38,21 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
+
+// Data sanitization against XSS
+app.use(xss());
+
+app.use(compression());
 
 //Routes
 app.use('/', viewRouter);
